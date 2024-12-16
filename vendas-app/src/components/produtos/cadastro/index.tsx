26ceaaa45da141c82c0619f94f7cca3e9a0841f0
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2024-12-16 16:46:21 -0300

    Cadastro de Produtos 2.0

--- vendas-app/src/components/produtos/cadastro/index.tsx
+++ vendas-app/src/components/produtos/cadastro/index.tsx
@@ -4,31 +4,40 @@ export const CadastroProdutos: React.FC = () => {
     return (
         <Layout titulo="Cadastro de Produtos">
 
-            <div className="field">
-                <label className="label" htmlFor="inputSku">SKU: *</label>
-                <div className="control">
-                    <input className="input" id="inputSku" placeholder="Insira o SKU do Produto"></input>
+            <div className="columns">
+
+                <div className="field is-half column">
+                    <label className="label" htmlFor="inputSku">SKU: *</label>
+                    <div className="control">
+                        <input className="input" id="inputSku" placeholder="Insira o SKU do Produto"></input>
+                    </div>
                 </div>
+
+                <div className="field is-half column">
+                    <label className="label" htmlFor="inputPreco">Preço: *</label>
+                    <div className="control">
+                        <input className="input" id="inputPreco" placeholder="Insira o Preço do Produto"></input>
+                    </div>
+                </div>
+
             </div>
 
-            <div className="field">
-                <label className="label" htmlFor="inputPreco">Preço: *</label>
-                <div className="control">
-                    <input className="input" id="inputPreco" placeholder="Insira o Preço do Produto"></input>
+            <div className="columns">
+
+                <div className="field column is-full">
+                    <label className="label" htmlFor="inputNome">Nome: *</label>
+                    <div className="control">
+                        <input className="input" id="inputNome" placeholder="Insira o Nome do Produto"></input>
+                    </div>
                 </div>
             </div>
+            <div className="columns">
 
-            <div className="field">
-                <label className="label" htmlFor="inputNome">Nome: *</label>
-                <div className="control">
-                    <input className="input" id="inputNome" placeholder="Insira o Nome do Produto"></input>
-                </div>
-            </div>
-
-            <div className="field">
-                <label className="label" htmlFor="inputDesc">Descricao: *</label>
-                <div className="control">
-                    <textarea className="textarea" id="inputDesc" placeholder="Insira a Descrição do Produto"></textarea>
+                <div className="field column is-full">
+                    <label className="label" htmlFor="inputDesc">Descricao: *</label>
+                    <div className="control">
+                        <textarea className="textarea" id="inputDesc" placeholder="Insira a Descrição do Produto"></textarea>
+                    </div>
                 </div>
             </div>
 
