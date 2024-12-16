a2517cc5317d3c481d6aed48e689622254a87829
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2024-12-16 15:24:59 -0300

    Criando Next

---
+++ vendas-app/src/app/cadastro/page.tsx
@@ -0,0 +1,7 @@
+export default function HelloPage() {
+    return (
+        <div>
+            <h1>Bem-vindo à página Cadastro</h1>
+        </div>
+    );
+}
---
+++ vendas-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Componente from "./meu-componente"
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <Componente />
+    </div>
+  );
+}
+
+export default Home;
