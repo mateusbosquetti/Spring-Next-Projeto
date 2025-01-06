40325480e664a0f1555576575e86e06241763da5
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-01-06 18:25:15 -0300

    Update form.tsx

--- vendas-app/src/components/clientes/cadastro/form.tsx
+++ vendas-app/src/components/clientes/cadastro/form.tsx
@@ -1,5 +1,6 @@
-import { Cliente } from "pasta/models/clientes" 
+import { Cliente } from "pasta/models/clientes"
 import { Formik, useFormik } from "formik"
+import { Input } from "components/common"
 
 interface ClienteFormProps {
     cliente: Cliente
@@ -20,22 +21,104 @@ export const ClienteForm: React.FC<ClienteFormProps> = ({
         id: '',
         nome: '',
         telefone: '',
-    
+
     }
 
     const formik = useFormik<Cliente>({
-         initialValues: {...cliente, ...formScheme},
-         onSubmit
+        initialValues: { ...formScheme, ...cliente },
+        onSubmit
     })
 
     return (
         <form onSubmit={formik.handleSubmit}>
-            <input value={formik.values.nome} 
-            onChange={formik.handleChange} 
-            id="nome"
-            name="nome"
-            />
-            <button type="submit">Enviar</button>
+            {formik.values.id &&
+                <div className="columns">
+                    <Input
+                        id="id"
+                        name="id"
+                        label="Código: "
+                        autoComplete="off"
+                        columnClasses="is-half"
+                        disabled
+                        value={formik.values.id} />
+
+                    <Input
+                        id="cadastro"
+                        name="cadastro"
+                        label="Data de Cadastro: "
+                        autoComplete="off"
+                        columnClasses="is-half"
+                        disabled
+                        value={formik.values.cadastro} />
+                </div>
+            }
+
+            <div className="columns">
+                <Input
+                    id="nome"
+                    name="nome"
+                    label="Nome *"
+                    autoComplete="off"
+                    columnClasses="is-full"
+                    onChange={formik.handleChange}
+                    value={formik.values.nome} />
+            </div>
+            <div className="columns">
+                <Input
+                    id="cpf"
+                    name="cpf"
+                    label="CPF *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.cpf} />
+
+                <Input
+                    id="dataNascimento"
+                    name="dataNascimento"
+                    label="Nascimento *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.dataNascimento} />
+            </div>
+            <div className="columns">
+                <Input
+                    id="endereco"
+                    name="endereco"
+                    label="Endereco *"
+                    autoComplete="off"
+                    columnClasses="is-full"
+                    onChange={formik.handleChange}
+                    value={formik.values.endereco} />
+            </div>
+            <div className="columns">
+                <Input
+                    id="email"
+                    name="email"
+                    label="Email *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.email} />
+
+                <Input
+                    id="telefone"
+                    name="telefone"
+                    label="Telefone *"
+                    autoComplete="off"
+                    columnClasses="is-half"
+                    onChange={formik.handleChange}
+                    value={formik.values.telefone} />
+            </div>
+            <div className="field is-grouped">
+                <div className="control is-link">
+                    <button
+                        type="submit" className="button is-link">{formik.values.id ? "Atualizar" : "Salvar"}
+                    </button>
+                    
+                </div>
+            </div>
         </form>
     )
 }
