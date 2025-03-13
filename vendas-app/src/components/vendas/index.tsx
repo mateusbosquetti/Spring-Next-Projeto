f0095ff86b88d641754d13cb59953ffe6c313634
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-12 21:51:59 -0300

    Update index.tsx

--- vendas-app/src/components/vendas/index.tsx
+++ vendas-app/src/components/vendas/index.tsx
@@ -1,19 +1,112 @@
 "use client";
 
 import React, { useEffect, useState, useCallback } from "react";
-import { Autocomplete, TextField, Stack, Button } from "@mui/material";
+import {
+  Autocomplete,
+  TextField,
+  Stack,
+  Button,
+  Box,
+  IconButton,
+} from "@mui/material";
 import { Layout } from "components";
 import { useClientService } from "pasta/services";
 import { useProdutoService } from "pasta/services";
 import { Cliente } from "pasta/models/clientes";
 import { Produto } from "pasta/models/produtos";
 import { AxiosResponse } from "axios";
-import useSWR from "swr";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import useSWR, { mutate } from "swr";
 import { httpCliente } from "pasta/http";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const fetcher = (url: string) => httpCliente.get(url);
 
 export const Vendas: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [produtoToDelete, setProdutoToDelete] = useState<string | null>(null);
+
+  const columns: GridColDef<Produto>[] = [
+    { field: "id", headerName: "ID", width: 50 },
+    {
+      field: "nome",
+      headerName: "Nome",
+      sortable: true,
+      width: 200,
+    },
+    {
+      field: "descricao",
+      headerName: "Descrição",
+      width: 300,
+    },
+    {
+      field: "preco",
+      headerName: "Preço",
+      type: "number",
+      sortable: true,
+      align: "left",
+      headerAlign: "left",
+      width: 150,
+    },
+    {
+      field: "sku",
+      headerName: "SKU",
+      sortable: false,
+      width: 150,
+    },
+    {
+      field: "dataCadastro",
+      headerName: "Data Cadastrado",
+      sortable: true,
+      width: 150,
+    },
+    {
+      field: "actions",
+      headerName: "Ações",
+      width: 100,
+      sortable: false,
+      editable: false,
+      filterable: false,
+      hideable: false,
+      disableReorder: true,
+      renderCell: (params) => {
+        return (
+          <>
+            <IconButton
+              color="error"
+              onClick={() => handleOpenModal(params.row.id || "")}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </>
+        );
+      },
+    },
+  ];
+
+  const handleOpenModal = (id: string) => {
+    setProdutoToDelete(id);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setProdutoToDelete(null);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (produtoToDelete) {
+      try {
+        await httpCliente.delete(`/api/produtos/${produtoToDelete}`);
+      } catch (error) {
+        console.error("Erro ao excluir produto:", error);
+      } finally {
+        handleCloseModal();
+      }
+    }
+  };
+
   // Produto
   const {
     data: produtoResult,
@@ -48,6 +141,7 @@ export const Vendas: React.FC = () => {
   const { listarProduto } = useProdutoService();
   const [listaCliente, setListaCliente] = useState<Cliente[]>([]);
   const [listaProduto, setListaProduto] = useState<Produto[]>([]);
+  const [listaProdutoPedido, setListaProdutoPedido] = useState<Produto[]>([]);
   const [sku, setSKU] = useState<string>("");
   const [valueCliente, setValueCliente] = useState<Cliente | null>(null);
   const [valueProduto, setValueProduto] = useState<Produto | null>(null);
@@ -83,10 +177,8 @@ export const Vendas: React.FC = () => {
         </div>
       </div>
       <div className="columns is-vcentered">
-        {/* Coluna para o Autocomplete e TextField */}
         <div className="column is-two-thirds">
           <div className="columns">
-            {/* Coluna do Autocomplete */}
             <div className="column is-half">
               <Autocomplete
                 {...produtoProps}
@@ -110,7 +202,6 @@ export const Vendas: React.FC = () => {
               />
             </div>
 
-            {/* Coluna do TextField (SKU) */}
             <div className="column is-half">
               <TextField
                 id="sku-field"
@@ -124,13 +215,33 @@ export const Vendas: React.FC = () => {
           </div>
         </div>
 
-        {/* Coluna do Botão */}
         <div className="column is-one-third has-text-right">
-          <Button variant="contained" color="primary">
+          <Button
+            onClick={(e) => {
+              
+            }}
+            variant="contained"
+            color="primary"
+          >
             ADICIONAR
           </Button>
         </div>
       </div>
+      <Box sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={listaProdutoPedido}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          disableRowSelectionOnClick
+        />
+      </Box>
     </Layout>
   );
 };
